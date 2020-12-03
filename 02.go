92cbc0fae7a6d3282e9b004963f08acd39903676
jlramos97 <jlramos97@gmail.com>
2020-12-04 00:14:26 +0100

    Updated description

--- 02.go
+++ 02.go
@@ -8,7 +8,7 @@
 
 // Each line gives the password policy and then the password. 
 // The password policy indicates the lowest and highest number of times a given letter must appear for the password to be valid. 
-// For example"
+// For example 1-3 a: abcde
 // 1-3 a means that the password must contain a at least 1 time and at most 3 times.
 
 package main
