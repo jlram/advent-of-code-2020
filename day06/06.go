66706d248d4f55774d0eb53564dc5338d0932794
jlramos97 <jlramos97@gmail.com>
2020-12-06 17:29:00 +0100

    Day 6 part 1

--- day06/06.go
+++ day06/06.go
@@ -2,6 +2,30 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
 
+func partOne(input []string) int {
+
+	count := 0
+	for _, group := range input {
+		m := make(map[string]int)
+		for _, letter := range strings.Replace(strings.Replace(group, "\r", "", -1), "\n", "", -1) {
+			if _, ok := m[string(letter)]; !ok {
+				m[string(letter)] = 0
+			}
+		}
+		count += len(m)
+	}
+	return count
+}
+
+func main() {
+	file, _ := ioutil.ReadFile("input.txt")
+	givenInput := strings.Split(string(file), "\n\r")
+
+	fmt.Println(partOne(givenInput))
 }
