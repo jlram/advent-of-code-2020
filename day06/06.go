ddac1c84634e160ddbde138220e102b7eeb2f9f7
jlramos97 <jlramos97@gmail.com>
2020-12-06 17:55:57 +0100

    Day 6 part 2

--- day06/06.go
+++ day06/06.go
@@ -9,7 +9,6 @@ import (
 )
 
 func partOne(input []string) int {
-
 	count := 0
 	for _, group := range input {
 		m := make(map[string]int)
@@ -23,9 +22,31 @@ func partOne(input []string) int {
 	return count
 }
 
+func partTwo(input []string) int {
+	count := 0
+	for _, group := range input {
+		m := make(map[string]int)
+		for _, letter := range strings.Replace(strings.Replace(group, "\r", "", -1), "\n", "", -1) {
+			if _, ok := m[string(letter)]; ok {
+				m[string(letter)]++
+			} else {
+				m[string(letter)] = 1
+			}
+		}
+
+		for _, value := range m {
+			if value == len(strings.Split(group, "\r\n")) {
+				count++
+			}
+		}
+	}
+	return count
+}
+
 func main() {
 	file, _ := ioutil.ReadFile("input.txt")
 	givenInput := strings.Split(string(file), "\n\r")
 
 	fmt.Println(partOne(givenInput))
+	fmt.Println(partTwo(givenInput))
 }
