24656572ff56804e919a932868494e90a81c648c
jlramos97 <jlramos97@gmail.com>
2020-12-08 11:34:54 +0100

    Day 7 part 2

--- day07/07.go
+++ day07/07.go
@@ -4,12 +4,13 @@ import (
 	"fmt"
 	"io/ioutil"
 	"regexp"
+	"strconv"
 	"strings"
 	"time"
 )
 
-func readBags(newBags []string, validBags map[string]bool, input []string) int {
-	reg, _ := regexp.Compile("(bags|bag)|[^a-zA-Z,]+")
+func readBagsOne(newBags []string, validBags map[string]bool, input []string) int {
+	reg := regexp.MustCompile("(bags|bag)|[^a-zA-Z,]+")
 	var aux []string // Var used to fill validBags each time
 
 	for _, v := range newBags { // Iterate new level of bags
@@ -28,10 +29,42 @@ func readBags(newBags []string, validBags map[string]bool, input []string) int {
 	// Check if there are new bags to iterate
 	if len(aux) > 0 {
 		newBags = aux
-		readBags(newBags, validBags, input)
+		readBagsOne(newBags, validBags, input)
 	}
 	// If not, return the number of bags that could eventually contain one shiny gold bag
 	return len(validBags)
+}
+
+func readBagsTwo(newBags []string, validBags map[string]int, mapBags map[string]string) int {
+	var aux []string // Var used to fill validBags each time
+	for _, v := range newBags {
+		totalValue := 1
+		for _, value := range strings.Split(mapBags[v], ",") {
+			re := regexp.MustCompile("[0-9]+")
+			charvalue := re.ReplaceAllString(value, "")
+			reFind := re.FindAllString(value, -1)
+			if len(reFind) > 0 {
+				numvalue, _ := strconv.Atoi(reFind[0])
+				validBags[charvalue] += numvalue
+				totalValue = totalValue * numvalue
+				fmt.Println(totalValue)
+				aux = append(aux, charvalue)
+			}
+		}
+	}
+
+	if len(aux) > 0 {
+		newBags = aux
+		readBagsTwo(newBags, validBags, mapBags)
+	}
+
+	total := 0
+	for _, v := range validBags {
+		total += v
+	}
+
+	// fmt.Println(validBags)
+	return total
 }
 
 func partOne(input []string) (int, time.Duration) {
@@ -39,9 +72,26 @@ func partOne(input []string) (int, time.Duration) {
 	newBags := []string{"shinygold"}
 	validBags := make(map[string]bool)
 
-	readBags(newBags, validBags, input)
+	readBagsOne(newBags, validBags, input)
 
 	return len(validBags), time.Since(start)
+}
+
+func partTwo(input []string) int {
+	newBags := []string{"shinygold"}
+	validBags := make(map[string]int)
+
+	mapBags := make(map[string]string)
+	reg := regexp.MustCompile("(bags|bag)|[^a-zA-Z0-9,]+")
+
+	for _, rule := range input {
+		elements := strings.Split(rule, "bags contain")
+		bag := strings.Replace(elements[0], " ", "", -1)
+		colors := reg.ReplaceAllString(elements[1], "")
+		mapBags[bag] = colors
+	}
+
+	return readBagsTwo(newBags, validBags, mapBags)
 }
 
 func main() {
@@ -49,5 +99,7 @@ func main() {
 	givenInput := strings.Split(string(file), "\n")
 
 	result, runtime := partOne(givenInput)
-	fmt.Printf("[#] Part 1: RESULT: %d RUNTIME: %s", result, runtime)
+	fmt.Printf("[#] Part 1: RESULT: %d RUNTIME: %s\n", result, runtime)
+
+	fmt.Println(partTwo(givenInput))
 }
