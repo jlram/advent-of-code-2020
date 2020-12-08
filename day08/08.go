ce59184514b4afc1f69dc5b801ce9cf8e3ab2f9b
jlramos97 <jlramos97@gmail.com>
2020-12-08 17:42:25 +0100

    Day 9 part 2

--- day08/08.go
+++ day08/08.go
@@ -44,42 +44,62 @@ func partOne(input []string) int {
 }
 
 func partTwo(input []string) int {
-	acc := 0
-	index := 0
-	var arr []int
-	exit := false
 	var possibleArr []int
+	acc := 0
 
 	for i, v := range input {
-		if strings.Contains(v, "nop") || strings.Contains(v, "nop") {
-			arr = append(possibleArr, i)
+		if strings.Contains(v, "nop") || strings.Contains(v, "jmp") {
+			possibleArr = append(possibleArr, i)
 		}
 	}
 
-	fmt.Println(possibleArr)
+	for _, v := range possibleArr {
+		acc = 0
+		index := 0
+		exit := false
+		var arr []int
 
-	for !exit {
-		if find(arr, index) {
-			exit = true
-		} else {
-			arr = append(arr, index)
-			value := strings.Split(input[index], " ")
-			if value[0] == "nop" {
-				index++
-			} else if number, _ := strconv.Atoi(strings.ReplaceAll(value[1], "\r", "")); value[0] == "acc" {
-				acc += number
-				index++
-			} else if value[0] == "jmp" {
-				index += number
+		// Changes "nop" to "jmp" and viceversa
+		if strings.Split(input[v], " ")[0] == "nop" {
+			input[v] = "jmp " + strings.Split(input[v], " ")[1]
+		} else if strings.Split(input[v], " ")[0] == "jmp" {
+			input[v] = "nop " + strings.Split(input[v], " ")[1]
+		}
+
+		for !exit {
+			if index == len(input)-1 { // If reaches the end of the input, returns acc value
+				return acc
+			}
+
+			if find(arr, index) {
+				exit = true // iterates next value
+			} else {
+				arr = append(arr, index)
+				value := strings.Split(input[index], " ")
+				if value[0] == "nop" {
+					index++
+				} else if number, _ := strconv.Atoi(strings.ReplaceAll(value[1], "\r", "")); value[0] == "acc" {
+					acc += number
+					index++
+				} else if value[0] == "jmp" {
+					index += number
+				}
 			}
 		}
+
+		// Reverse changes
+		if strings.Split(input[v], " ")[0] == "nop" {
+			input[v] = "jmp " + strings.Split(input[v], " ")[1]
+		} else if strings.Split(input[v], " ")[0] == "jmp" {
+			input[v] = "nop " + strings.Split(input[v], " ")[1]
+		}
 	}
-	return acc
+	return 0
 }
 
 func main() {
 	file, _ := ioutil.ReadFile("input.txt")
-	givenInput := strings.Split(string(file), "\n")
+	givenInput := strings.Split(string(file), "\r\n")
 
 	fmt.Println(partOne(givenInput))
 	fmt.Println(partTwo(givenInput))
